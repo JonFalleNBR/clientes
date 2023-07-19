672ef3368f9cd976e7d01a60296060466468d99b
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2023-07-18 22:58:38 -0300

    Metodos de FindById, Delete e Atualizar no Endpoint

--- apiclientes/src/main/java/io/github/jonfallenbr/apiclientes/rest/ClienteController.java
+++ apiclientes/src/main/java/io/github/jonfallenbr/apiclientes/rest/ClienteController.java
@@ -6,6 +6,7 @@ import io.github.jonfallenbr.apiclientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -22,5 +23,36 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente){
         return repository.save(cliente);
+    }
+
+
+    @GetMapping("{id}")
+    public Cliente AcharPorId(@PathVariable Integer id){
+        return repository.findById(id).
+                orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // Indica que não ha nenhum objeto de retorno, indicando que o data ja foi deletado do local
+    public void deletar(@PathVariable Integer id){
+        repository.findById(id)
+                    .map(cliente -> {
+                    repository.delete(cliente);
+                     return Void.TYPE;
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado){
+        repository.findById(id)
+                .map(cliente -> {
+                    clienteAtualizado.setId(cliente.getId());
+                    return repository.save(clienteAtualizado);
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
     }
 }
