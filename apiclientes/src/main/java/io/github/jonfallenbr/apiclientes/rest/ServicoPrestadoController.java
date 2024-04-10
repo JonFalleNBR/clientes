67f79904ab253600ec976136fe8fb0c3976c64ba
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2024-04-10 01:53:10 -0300

    Inclusão da Annotation CrossOrigin no ServicoPrestadoController para evitar problemas de CORS na requsição da Service no FrontEnd

--- apiclientes/src/main/java/io/github/jonfallenbr/apiclientes/rest/ServicoPrestadoController.java
+++ apiclientes/src/main/java/io/github/jonfallenbr/apiclientes/rest/ServicoPrestadoController.java
@@ -20,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/servicos-prestados")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class ServicoPrestadoController {
 
 
