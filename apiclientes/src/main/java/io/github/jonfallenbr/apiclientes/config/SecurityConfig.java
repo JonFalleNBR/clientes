103739f1771ae1e89666add93aeeabc69f33093a
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2024-04-16 22:28:56 -0300

    Customização dos acessos para criação de tokens para autenticação e acesso nas aplicações - Classes AuthorizationServerConfig, ResourceServerConfig e SecurityConfig

--- apiclientes/src/main/java/io/github/jonfallenbr/apiclientes/config/SecurityConfig.java
+++ apiclientes/src/main/java/io/github/jonfallenbr/apiclientes/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -35,4 +37,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
     }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
+
+/*
+Essa classe não necessita da anotation @Configuration, uma vez que o @EnableWebSecurity ja possui tal anotação e acesso as requisição de configuração.
+ */
