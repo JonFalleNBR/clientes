e38e1fc07f067a1238294e818342bb90f67ebe56
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2024-04-17 23:40:14 -0300

    Configuração JWT no application.properties e na AuthorizationServerConfig com uma senha codificada - Halo é o melhor jogo do mundo

--- apiclientes/src/main/java/io/github/jonfallenbr/apiclientes/config/AuthorizationServerConfig.java
+++ apiclientes/src/main/java/io/github/jonfallenbr/apiclientes/config/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package io.github.jonfallenbr.apiclientes.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +13,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -20,9 +23,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Value("${security.jwt.signing-key}")
+    private String signingKey;
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(signingKey);
+        return tokenConverter;
+    }
+
     @Bean
     public TokenStore tokenStore(){
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
     }
 
 
@@ -30,6 +43,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
             endpoints
                     .tokenStore(tokenStore())
+                    .accessTokenConverter(accessTokenConverter())
                     .authenticationManager(authenticationManager);
     }
 
@@ -50,4 +64,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(ClientDetailsServiceConfigurer clients) é de extrema importancia pois permite o acesso de outras aplicações, como do front end para acessar as configs de acesso e autenticação que estão aqui sendo estabelecidas
 
     Esse metodo padroniza as configurações de token que geralmente vem de maneira aleatoria quando subimos a aplicação e precisamos jogar no Postman ou Swagger afim de testarmos a funcionalidade da aplicação
+
+    -- Senha codificada no BASE64 e colada no application.properties - Halo é o melhor jogo do mundo
  */
